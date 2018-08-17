b8e36efcb7a2d9eb8c409cf74f0bbbc753a28928
Frank Ye(葉詠富) <frank_ye@asus.com>
2018-08-17 21:31:39 +0800

    struct

--- 第二章陣列與結構.c
+++ 第二章陣列與結構.c
@@ -19,17 +19,73 @@
 //  	return tempsum;
 //  }
 
-int one[]={0,1,2,3,4};
-void print1(int *ptr, int rows)
-{
-	int i;
-	printf("Address Contents\n");
-	for(i=0;i<rows;i++)
-		printf("%8u%5d\n",ptr+i,*(ptr+i));
-	printf("\n");
-}
+//int one[]={0,1,2,3,4};
+//void print1(int *ptr, int rows)
+//{
+//	int i;
+//	printf("Address Contents\n");
+//	for(i=0;i<rows;i++)
+//		printf("%8u%5d\n",ptr+i,*(ptr+i));
+//	printf("\n");
+//}
+//
+//void main(void){
+//	print1(&one[0],5);
+//	
+//}
+
+//struct
+//struct{
+//	char name[10];
+//	int age;
+//	float salary;
+//}person;
+//
+//strcpy(person.name,"frank");
+//person.age=10;
+//person.salary=1000;
+
+//typedef struct human_being{
+//	char name[10];
+//	int age;
+//	float salary;
+//};
+
+struct human_being{
+	char name[10];
+	int age;
+	float salary;
+};
+
 
 void main(void){
-	print1(&one[0],5);
+	struct human_being person1;
+	struct human_being person2;
+	
+	printf("Please input your name : ");
+	gets(person1.name);
+	
+	printf("Please input your name : ");
+	gets(person2.name);
+	
+	if(strcmp(person1.name, person2.name)==0)
+		printf("Same name");
+	else
+		printf("Not the same name");
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
